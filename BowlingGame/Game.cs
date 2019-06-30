2cc7c778c35c09a35de33e286bd60aab4d47fbd8
Abdulla Mian <miana@claycorp.com>
2019-06-30 18:11:09 -0500

    three tests

---
+++ BowlingGame/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private int score = 0;
+
+        public void Rolls(int pinsHit)
+        {
+            score += pinsHit;
+
+        }
+
+        public int Score()
+        {
+            return score;
+        }
+    }
+}
---
+++ BowlingGameTests/BowlingGameTest.cs
@@ -0,0 +1,46 @@
+﻿using BowlingGame;
+using NUnit.Framework;
+using System;
+
+namespace BowlingGameTests
+{
+    
+    public class BowlingGameTest
+    {
+
+        [Test]
+        public void CreateGame()
+        {
+            Game g = new Game();
+        }
+
+        [Test]
+        public void GutterGame()
+        {
+            Game g = new Game();
+
+            for (int i = 0;  i < 20; i++)
+            {
+                g.Rolls(0);
+            }
+
+            Assert.AreEqual(0, g.Score());
+        }
+
+        [Test]
+        public void RollAllOnes()
+        {
+            Game g = new Game();
+
+            for (int i = 0; i < 20; i++)
+            {
+                g.Rolls(1);
+            }
+
+            Assert.AreEqual(20, g.Score());
+        }
+
+    
+
+    }
+}
