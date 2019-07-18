c49f69c62d12dd68f9122d3d9641c4a0845d9d09
miana <miana@claycorp.com>
2019-07-18 09:41:27 -0500

    completed kata

---
+++ BowlingGame/Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private int currentRoll = 0;
+        private int[] rolls = new int[21];
+
+        public void Roll(int pinsFall)
+        {
+            rolls[currentRoll++] = pinsFall;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+
+            for(int frame = 0; frame < 10; frame++)
+            {
+                if (isStrike(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else if (isSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool isStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+
+        private bool isSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+    }
+}
