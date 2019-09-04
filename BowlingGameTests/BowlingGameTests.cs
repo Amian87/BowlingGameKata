7a0b5f5055f67418ed015d1cb28134ff3c18e91c
miana <miana@claycorp.com>
2019-09-03 20:48:05 -0500

    complete

---
+++ BowlingGameTests/BowlingGameTests.cs
@@ -0,0 +1,65 @@
+﻿using BowlingGame;
+using NUnit.Framework;
+using System;
+
+namespace BowlingGameTests
+{
+    public class BowlingGameTests
+    {
+        Game game = new Game();
+
+        [SetUp]
+        public void setUpGame()
+        {
+            game = new Game();
+        }
+
+        public void rollMany(int numberOfRolls, int pinsFall)
+        {
+            for(int i = 0; i < numberOfRolls; i++)
+            {
+                game.Roll(pinsFall);
+            }
+        }
+
+        [Test]
+        public void gutterGame()
+        {
+            rollMany(20, 0);
+            Assert.AreEqual(0, game.Score());
+        }
+
+        [Test]
+        public void rollAllOnes()
+        {
+            rollMany(20, 1);
+            Assert.AreEqual(20, game.Score());
+        }
+
+        [Test]
+        public void rollOneSpare()
+        {
+            rollMany(2, 5);
+            rollMany(1, 3);
+            rollMany(17, 0);
+            Assert.AreEqual(16, game.Score());
+        }
+
+        [Test]
+        public void rollOneStrike()
+        {
+            rollMany(1, 10);
+            rollMany(1, 3);
+            rollMany(1, 4);
+            rollMany(16, 0);
+            Assert.AreEqual(24, game.Score());
+        }
+
+        [Test]
+        public void perfectGame()
+        {
+            rollMany(12, 10);
+            Assert.AreEqual(300, game.Score());
+        }
+    }
+}
