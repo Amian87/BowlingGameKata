4f8303d39481e514fabef3b78a3d05ef4b16cb68
Abdulla Mian <miana@claycorp.com>
2019-09-26 10:25:43 -0500

    check in

---
+++ BowlingGame/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private int currentScore = 0;
+        private int[] rolls = new int[21];
+
+        public void Roll(int pinsFall)
+        {
+            rolls[currentScore++] += pinsFall;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int i = 0;
+
+            for(int frame = 0; frame < 10; frame++)
+            {
+                if(rolls[i] == 10)
+                {
+                    score += 10 + rolls[i + 2];
+                    i += 2;
+                }
+                else
+                {
+                    score += rolls[i] + rolls[i + 1];
+                    i += 2;
+                }
+            }
+
+            return score;
+        }
+
+    }
+}
---
+++ BowlingGameTests/BowlingGameTests.cs
@@ -0,0 +1,48 @@
+﻿using BowlingGame;
+using NUnit.Framework;
+using System;
+
+namespace BowlingGameTests
+{
+    public class BowlingGameTests
+    {
+        private Game game = new Game();
+
+        [SetUp]
+        public void SetUpGame()
+        {
+            game = new Game();
+        }
+
+        public void RollMany(int numberOfRolls, int pinsFall)
+        {
+            for(int i = 0; i < numberOfRolls; i++)
+            {
+                game.Roll(pinsFall);
+            }
+        }
+
+        [Test]
+        public void GutterGame()
+        {
+            RollMany(20, 0);
+            Assert.AreEqual(0, game.Score());
+        }
+
+        [Test]
+        public void RollAllOnes()
+        {
+            RollMany(20, 1);
+            Assert.AreEqual(20, game.Score());
+        }
+
+        [Test]
+        public void RollOneSpare()
+        {
+            RollMany(2, 5);
+            RollMany(1, 3);
+            RollMany(17, 0);
+            Assert.AreEqual(16, game.Score());
+        }
+    }
+}
