e1f1697e6d2c83dca77e6b6920e1cae15316ed0a
Abdulla Mian <miana@claycorp.com>
2019-12-16 00:13:09 -0600

    completed

---
+++ BowlingGame/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private int _currentScore = 0;
+        private readonly int[] _roll = new int[21];
+
+        public void Roll(int pinsFall)
+        {
+            _roll[_currentScore++] += pinsFall;
+        }
+
+        public int Score()
+        {
+            var score = 0;
+            var frameIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (_roll[frameIndex] == 10)
+                {
+                    score += 10 + _roll[frameIndex + 1] + _roll[frameIndex + 2];
+                    frameIndex += 1;
+                }
+                else if (_roll[frameIndex] + _roll[frameIndex + 1] == 10)
+                {
+                    score += 10 + _roll[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += _roll[frameIndex] + _roll[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+
+
+            return score;
+        }
+    }
+}
---
+++ BowlingGameTests/BowlingGameTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using BowlingGame;
+using NUnit.Framework;
+
+namespace BowlingGameTests
+{
+    public class BowlingGameTest
+    {
+        private Game game;
+
+        [SetUp]
+        public void SetUpGame()
+        {
+            game = new Game();
+        }
+
+        public void RollManyTimes(int numberOfRolls, int pinsFall)
+        {
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                game.Roll(pinsFall);
+            }
+        }
+
+        [Test]
+        public void GutterGame()
+        {
+            RollManyTimes(20, 0);
+            Assert.AreEqual(0, game.Score());
+        }
+
+        [Test]
+        public void RollAllOnes()
+        {
+            RollManyTimes(20,1);
+            Assert.AreEqual(20, game.Score());
+        }
+
+        [Test]
+        public void RollOneSpare()
+        {
+            RollManyTimes(2,5);
+            RollManyTimes(1,3);
+            RollManyTimes(17, 0);
+            Assert.AreEqual(16, game.Score());
+        }
+
+        [Test]
+        public void RollOneStrike()
+        {
+            RollManyTimes(1, 10);
+            RollManyTimes(1, 3);
+            RollManyTimes(1, 4);
+            RollManyTimes(16, 0);
+            Assert.AreEqual(24, game.Score());
+        }
+
+        [Test]
+        public void PerfectGame()
+        {
+            RollManyTimes(20, 10);
+            Assert.AreEqual(300, game.Score());
+        }
+
+
+    }
+}
