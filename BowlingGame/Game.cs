8cec11dbdb6129b91d39ad96396ff1b3d30db926
Abdulla Mian <miana@claycorp.com>
2020-05-03 07:23:47 -0500

    complete

---
+++ BowlingGame/Game.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private int[] roll = new int[21];
+        private int currentScore = 0;
+
+        public void Roll(int pinsFall)
+        {
+            roll[currentScore++] += pinsFall;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if(IsStrike(frameIndex))
+                {
+                    score += 10 + roll[frameIndex + 1] + roll[frameIndex + 2];
+                    frameIndex += 1;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += 10 + roll[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += roll[frameIndex] + roll[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return roll[frameIndex] + roll[frameIndex + 1] == 10;
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return roll[frameIndex] == 10;
+        }
+    }
+}
