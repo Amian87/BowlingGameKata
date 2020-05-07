45c70994d799d3046be17955f2fe08547ce0f1e3
Abdulla Mian <miana@claycorp.com>
2020-05-07 01:11:50 -0500

    complete

---
+++ BowlingGame/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private readonly int[] _roll = new int[21];
+        private int currentScore = 0;
+
+        public void Roll(int pinsFall)
+        {
+            _roll[currentScore++] += pinsFall;
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+
+            for(int frame = 0; frame < 10; frame++)
+            {
+                if (_roll[frameIndex] == 10)
+                {
+                    score += 10 + _roll[frameIndex + 1] + _roll[frameIndex + 2];
+                    frameIndex += 1;
+                }
+                else if (_roll[frameIndex] + _roll[frameIndex + 1] == 10)
+                {
+                    score += 10 + _roll[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += _roll[frameIndex] + _roll[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+            
+            return score;
+        }
+    }
+}
